c98f1ada2598485809a995ec2b7d9389237615e2
yag <marks.yag@gmail.com>
2020-04-30 22:31:30 +0800

    Reduce log level.

--- src/main/kotlin/com/github/yag/retry/Retry.kt
+++ src/main/kotlin/com/github/yag/retry/Retry.kt
@@ -66,7 +66,7 @@ class Retry(
                     retryCount++
                     continue
                 }
-                LOG.warn("Give up {} after {} retries, error: {}.", name, retryCount, t.toString())
+                LOG.info("Give up {} after {} retries, error: {}.", name, retryCount, t.toString())
                 throw t
             }
         }
