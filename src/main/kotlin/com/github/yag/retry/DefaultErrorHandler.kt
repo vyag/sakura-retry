6dbe2f7794e2c53375987e4cb84654b279ad5194
yag <marks.yag@gmail.com>
2020-10-26 23:12:59 +0800

    Optimize logging of DefaultErrorHandler.kt

--- src/main/kotlin/com/github/yag/retry/DefaultErrorHandler.kt
+++ src/main/kotlin/com/github/yag/retry/DefaultErrorHandler.kt
@@ -25,20 +25,42 @@ open class DefaultErrorHandler @JvmOverloads constructor(@Value private val logS
     ErrorHandler {
 
     override fun handle(retryCount: Int, duration: Duration, error: Throwable, allowRetry: Boolean, backOffDuration: Duration) {
+        val printStack = isStacktraceRequired(error)
         val durationMs = duration.toMillis()
-        if (durationMs > logSuppressTimeMs || isUnexpected(error)) {
-            if (allowRetry) {
-                LOG.info("Invocation failed, retryCount: {}, duration: {}ms, will retry in {}ms.", retryCount,
-                    durationMs, backOffDuration.toMillis(), error)
+        if (isUnexpected(error)) {
+            if (printStack) {
+                LOG.warn("Unexpected invocation failed, retryCount: {}, duration: {}ms.", retryCount, durationMs, error)
             } else {
-                LOG.info("Invocation failed, retryCount: {}, duration: {}ms.", retryCount, durationMs, error)
+                LOG.warn("Unexpected invocation failed: $error retryCount: {}, duration: {}ms.", retryCount, durationMs)
             }
         } else {
-            LOG.debug("Invocation failed, retryCount: {}, duration: {}ms.", retryCount, durationMs, error)
+            if (durationMs > logSuppressTimeMs) {
+                if (allowRetry) {
+                    if (printStack) {
+                        LOG.info(
+                            "Invocation failed, retryCount: {}, duration: {}ms, will retry in {}ms.", retryCount,
+                            durationMs, backOffDuration.toMillis(), error
+                        )
+                    } else {
+                        LOG.info(
+                            "Invocation failed: $error retryCount: {}, duration: {}ms, will retry in {}ms.", retryCount,
+                            durationMs, backOffDuration.toMillis()
+                        )
+                    }
+                } else {
+                    if (printStack) {
+                        LOG.info("Invocation failed, retryCount: {}, duration: {}ms.", retryCount, durationMs, error)
+                    } else {
+                        LOG.info("Invocation failed: $error retryCount: {}, duration: {}ms.", retryCount, durationMs)
+                    }
+                }
+            }
         }
     }
 
     open fun isUnexpected(t: Throwable) = false
+
+    open fun isStacktraceRequired(t: Throwable) = true
 
     companion object {
         private val LOG = LoggerFactory.getLogger(DefaultErrorHandler::class.java)
