811796827277025e74cc3a1adfb7288515fa7af2
yag <marks.yag@gmail.com>
2025-06-23 21:52:57 +0800

    Change package name.

---
+++ core/src/main/kotlin/sakura/retry/BackoffPolicy.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018-2020 marks.yag@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package sakura.retry
+
+import java.time.Duration
+
+/**
+ * Backoff policy.
+ */
+fun interface BackoffPolicy {
+
+    /**
+     * Returns the backoff duration.
+     *
+     * @param context the context
+     * @return the backoff duration
+     */
+    fun backoff(context: Context): Duration
+    
+    /**
+     * Returns a new backoff policy that is the sum of this backoff policy and the given backoff policy.
+     *
+     * @param backoffPolicy the backoff policy to add
+     * @return a new backoff policy that is the sum of this backoff policy and the given backoff policy
+     */
+    infix operator fun plus(backoffPolicy: BackoffPolicy): BackoffPolicy {
+        val self = this
+        return BackoffPolicy { context -> self.backoff(context) + backoffPolicy.backoff(context) }
+    }
+}
+
+
---
+++ demo/src/main/kotlin/sakura/retry/demo/kotlin/Async.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025-2025 marks.yag@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package sakura.retry.demo.kotlin
+
+import sakura.retry.BackoffPolicies.fixedDelayInSeconds
+import sakura.retry.Context
+import sakura.retry.RetryTemplate
+import sakura.retry.RetryPolicies
+import java.io.IOException
+import java.time.Duration
+import java.util.*
+import java.util.concurrent.Executors
+
+fun main() {
+    val random = Random(System.currentTimeMillis())
+    Executors.newScheduledThreadPool(4).use { executor ->
+        val policy = RetryTemplate.Builder(RetryPolicies.TRUE, fixedDelayInSeconds(1))
+            .addFailureListener { call: String?, context: Context, allowRetry: Boolean, backOffDuration: Duration ->
+                println("Call $call, attempt ${context.attemptCount} failed: (${context.failure.message})")
+            }.build()
+        (0 until 3).map { 
+            policy.submit(executor, "call$it") {
+                random.nextDouble(10.0).takeUnless { it < 7 } ?: throw IOException("Too small")    
+            } 
+        }.map { 
+            it.thenAccept(::println)
+        }.forEach {
+            it.join()
+        }
+    }
+}
